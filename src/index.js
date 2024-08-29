a33d7b5e1e39643516f0e771bb7c3e033081fe6f
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-08-29 20:44:59 +0530

    yes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
 
+import ReactDOM from 'react-dom';
 
+import App from './components/App';
 
+ 
 
+ 
 
-createRoot(document.getElementById('root')).render(<App/>)
+ReactDOM.render(<App />, document.getElementById('root'));
